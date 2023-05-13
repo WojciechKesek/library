e959ff2c6eb77adec10bf2a16f40f686828eeca9
Wojciech Kesek <wojtekesek@gmail.com>
2023-05-13 11:24:38 +0200

    first commit on calculator

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         Calculator calc = new Calculator();
-        calc.calculate(1, 2);
+        System.out.println(calc.calculate(1, 2));
     }
 }
