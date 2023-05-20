68d049d4165971124f44d3fa6a9035b48de4dc0a
Wojciech Kesek <wojtekesek@gmail.com>
2023-05-20 09:07:05 +0200

    add Book search

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,13 +1,22 @@
 package com.sda;
 
 
+import com.sda.view.AllBooksView;
+import com.sda.view.BooksByAuthorView;
 import com.sda.view.LoginView;
 import com.sda.view.View;
 
 public class Main {
 
     public static void main(String[] args) {
+        View booksView = new BooksByAuthorView();
+        booksView.display();
         View view = new LoginView();
         view.display();
+
     }
+
+    // TODO wyświetlanie opcji po kolei niezależnie od roli, uniwersalnie nie tylko dla menu,
+    //  ale też ksiązek i użytkowników (typ generyczny)
+
 }
---
+++ src/main/java/com/sda/controller/BookController.java
@@ -0,0 +1,12 @@
+package com.sda.controller;
+
+import com.sda.model.Author;
+import com.sda.model.Book;
+
+import java.util.Map;
+
+public interface BookController {
+    Map<Integer, Book> getAllBooks();
+
+    Map<Integer, Book> getBooksByAuthor(Author author);
+}
